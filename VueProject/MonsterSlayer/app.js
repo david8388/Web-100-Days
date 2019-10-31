417618e8ada9a923edd6bd84c938c3e5acdbe47b
David <hsienwu830808@gmail.com>
2019-10-31 22:46:36 +0800

    Refactoring attack methods

--- VueProject/MonsterSlayer/app.js
+++ VueProject/MonsterSlayer/app.js
@@ -12,30 +12,37 @@ new Vue({
       this.monsterHealth = 100;
     },
     attack: function() {
-      let max = 10;
-      let min = 3;
-      let damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-      this.monsterHealth -= damage;
+      this.monsterHealth -= this.calculateDamage(3, 10);
 
-      if (this.monsterHealth <= 0) {
-        alert("You won");
-        this.gameIsRunning = false;
+      if (this.checkWin()) {
         return;
       }
-
-      max = 12;
-      min = 3;
-      damage = Math.max(Math.floor(Math.random() * 12) + 1, min);
-      this.playerHealth -= damage;
-
-      if (this.playerHealth <= 0) {
-        alert("You lost");
-        this.gameIsRunning = false;
-        return;
-      }
+      this.playerHealth -= this.calculateDamage(3, 12);
+      this.checkWin();
     },
     specialAttack: function() {},
     heal: function() {},
-    giveUp: function() {}
+    giveUp: function() {},
+    calculateDamage: function(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkWin: function() {
+      if (this.monsterHealth <= 0) {
+        if (confirm("You won! New Game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm("You lost! New Game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    }
   }
 });
