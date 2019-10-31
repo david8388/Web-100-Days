74a68e3ef5b4515b6b11e77e28634032cce150fd
David <hsienwu830808@gmail.com>
2019-11-01 06:50:26 +0800

    Implementing a heal method

--- VueProject/MonsterSlayer/app.js
+++ VueProject/MonsterSlayer/app.js
@@ -27,7 +27,15 @@ new Vue({
       }
       this.monsterAttacks();
     },
-    heal: function() {},
+    heal: function() {
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth = 100;
+      }
+
+      this.monsterAttacks();
+    },
     giveUp: function() {},
     monsterAttacks: function() {
       this.playerHealth -= this.calculateDamage(3, 12);
