07f31e4eb041be9720885f3f9c75d01e6581332a
David <hsienwu830808@gmail.com>
2019-11-02 10:08:21 +0800

    Finishing the log

--- VueProject/MonsterSlayer/app.js
+++ VueProject/MonsterSlayer/app.js
@@ -11,6 +11,7 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function() {
       const damage = this.calculateDamage(3, 10);
@@ -25,7 +26,12 @@ new Vue({
       this.monsterAttacks();
     },
     specialAttack: function() {
-      this.monsterHealth -= this.calculateDamage(10, 20);
+      const damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: `Player hits Monster hard for ${damage}`
+      });
       if (this.checkWin()) {
         return;
       }
@@ -37,6 +43,11 @@ new Vue({
       } else {
         this.playerHealth = 100;
       }
+
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Player heals for 10"
+      });
 
       this.monsterAttacks();
     },
