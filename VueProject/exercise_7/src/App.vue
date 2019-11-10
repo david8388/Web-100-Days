b89fcb31294933172b5c35a062e512e61112717b
David <hsienwu830808@gmail.com>
2019-11-10 15:48:29 +0800

    Init exercise_7

---
+++ VueProject/exercise_7/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <app-user></app-user>
+  </div>
+</template>
+
+<script>
+import User from './components/User.vue';
+
+export default {
+  components: {
+    appUser: User
+  }
+}
+</script>
+
+<style>
+div.component {
+  border: 1px solid black;
+  padding: 30px;
+}
+</style>
