5a4a842c8c54498354095addb8d97e34f4af73c9
David <hsienwu830808@gmail.com>
2019-11-13 06:37:24 +0800

    Dynamic component behavior

--- VueProject/AdvancedComponent/src/components/New.vue
+++ VueProject/AdvancedComponent/src/components/New.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
     <p>New Quote</p>
+    <button @click="counter++">Increase</button>
+    <p>{{ counter }}</p>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: function () {
+    return {
+      counter: 0
+    }
+  },
+  destroyed () {
+    console.log('Destroyed!');
+  }
 }
 </script>
 
