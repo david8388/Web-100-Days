daf2bfe38f5a1a8635e369cd48a775df59390b03
David <hsienwu830808@gmail.com>
2019-11-14 21:19:02 +0800

    Init the application

---
+++ VueProject/wonderful-quote/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      quotes: ['Just a quote to see something'],
+      maxQuotes: 10
+    }
+  }
+}
+</script>
+
+<style>
+</style>
