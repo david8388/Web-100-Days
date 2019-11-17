31cf01a086d450461ff3adab283beb2c05e670cf
David <hsienwu830808@gmail.com>
2019-11-17 11:12:42 +0800

    Adding header component

---
+++ VueProject/wonderful-quote/src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12">
+      <h3>Quote Added</h3>
+      <div class="progress">
+        <div
+          class="progress-bar"
+          role="progressbar"
+          aria-valuenow="60"
+          aria-valuemin="0"
+          aria-valuemax="100"
+          :style="{width:  quoteCount / maxQuotes * 100 + '%'}"
+        >
+          {{ quoteCount }} / {{ maxQuotes }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    quoteCount: {
+      type: Number,
+    },
+    maxQuotes: {
+      type: Number
+    }
+  }
+}
+</script>
+
+<style>
+</style>
