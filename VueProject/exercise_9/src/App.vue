4895ce4971d39e9871e179dffdf959a12a9a90bc
David <hsienwu830808@gmail.com>
2019-11-24 10:43:29 +0800

    Add exercise_9
    Signed-off-by: David <hsienwu830808@gmail.com>

---
+++ VueProject/exercise_9/src/App.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="container">
+    <form>
+      <div
+        class="row"
+        v-if="!submitted"
+      >
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <app-full-name :value="userData.fullName"></app-full-name>
+          <div class="form-group">
+            <label for="mail">Mail</label>
+            <input
+              type="text"
+              class="form-control"
+              id="mail"
+              v-model="userData.mail"
+            >
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="password"
+              v-model="userData.password"
+            >
+          </div>
+          <div class="form-group">
+            <label for="storeData">Store Data</label>
+            <input
+              type="radio"
+              id="male"
+              value="yes"
+              v-model="userData.isStore"
+              @click="userData.isStore = 'yes'"
+            /> Yes
+
+            <input
+              type="radio"
+              id="male"
+              value="no"
+              v-model="userData.isStore"
+              @click="userData.isStore = 'no'"
+            /> No
+          </div>
+          <button
+            type="submit"
+            @click.prevent="submitted = true"
+            class="btn btn-primary"
+          >Submit the form</button>
+        </div>
+      </div>
+    </form>
+    <hr>
+    <div
+      class="row"
+      v-if="submitted"
+    >
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4>Your Data</h4>
+          </div>
+          <div class="panel-body">
+            <p>Full Name: {{ userData.fullName }}</p>
+            <p>Mail: {{ userData.mail }}</p>
+            <p>Password: {{ userData.password }}</p>
+            <p>Store in Database?: {{ userData.isStore }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import FullName from './FullName.vue';
+
+export default {
+  data () {
+    return {
+      userData: {
+        fullName: 'David Wu',
+        mail: '',
+        password: '',
+        isStore: 'no'
+      },
+      submitted: false
+    }
+  },
+  components: {
+    appFullName: FullName
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ VueProject/exercise_9/src/FullName.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="form-group">
+    <label for="firstName">First Name</label>
+    <input
+      type="text"
+      class="form-control"
+      :value="firstName"
+      @input="nameChanged(true, $event)"
+    >
+    <label for="lastName">Last Name</label>
+    <input
+      type="text"
+      class="form-control"
+      :value="lastName"
+      @input="nameChanged(false, $event)"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String
+    }
+  },
+  methods: {
+    nameChanged: function (isFirst, event) {
+      let name = ''
+      if (isFirst) {
+        name = event.target.value + ' ' + this.lastName
+      } else {
+        name = this.firstName + ' ' + event.target.valuesx
+      }
+      this.value = name
+      this.$emit('input', name)
+    }
+  },
+  computed: {
+    firstName () {
+      return this.value.split(' ')[0]
+    },
+    lastName () {
+      return this.value.split(' ')[1]
+    },
+  }
+}
+</script>
