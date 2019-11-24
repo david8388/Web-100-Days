8e2be364870a686535b200bdd017c5dbb0dc8356
David <hsienwu830808@gmail.com>
2019-11-25 07:18:09 +0800

    Init directives project
    Signed-off-by: David <hsienwu830808@gmail.com>

---
+++ VueProject/directives/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Build-in Directives</h1>
+        <p v-text="'Some text'"></p>
+        <p v-html="'<strong>Some Strong text</strong>'"></p>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p>Color this</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
