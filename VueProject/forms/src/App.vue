81fc30dcea1080e663e9cb159c45c0d2af0752d8
David <hsienwu830808@gmail.com>
2019-11-24 08:37:01 +0800

    Submitting a form
    Signed-off-by: David <hsienwu830808@gmail.com>

--- VueProject/forms/src/App.vue
+++ VueProject/forms/src/App.vue
@@ -114,12 +114,18 @@
       <hr />
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-          <button class="btn btn-primary">Submit!</button>
+          <button
+            class="btn btn-primary"
+            @click.prevent="submitted"
+          >Submit!</button>
         </div>
       </div>
     </form>
     <hr />
-    <div class="row">
+    <div
+      class="row"
+      v-if="isSubmitted"
+    >
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <div class="panel panel-default">
           <div class="panel-heading">
@@ -165,7 +171,13 @@ export default {
       gender: 'Male',
       priorities: ['Hight', 'Medium', 'Low'],
       selectedPriority: 'High',
-      dataSwitch: true
+      dataSwitch: true,
+      isSubmitted: false
+    }
+  },
+  methods: {
+    submitted: function () {
+      this.isSubmitted = true
     }
   },
   components: {
