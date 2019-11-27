7a7d9c0b667cb100067056ce6e7509ed6db5c8d2
David <hsienwu830808@gmail.com>
2019-11-28 07:23:47 +0800

    Creating a local filter
    Signed-off-by: David <hsienwu830808@gmail.com>

---
+++ VueProject/filter_mixins/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters & Mixins</h1>
+        <p>{{ text | toUpperCase }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      text: 'Hello World'
+    }
+  },
+  filters: {
+    toUpperCase (value) {
+      return value.toUpperCase()
+    }
+  }
+}
+</script>
+
+<style>
+</style>
