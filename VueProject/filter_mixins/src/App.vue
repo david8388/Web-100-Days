8ecf11a16258400af918dd96cad4142be61f70a5
David <hsienwu830808@gmail.com>
2019-11-29 07:11:38 +0800

    Global filters and chain multi filters
    Signed-off-by: David <hsienwu830808@gmail.com>

--- VueProject/filter_mixins/src/App.vue
+++ VueProject/filter_mixins/src/App.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
-        <p>{{ text | toUpperCase }}</p>
+        <p>{{ text | toUpperCase | to-lowercase}}</p>
       </div>
     </div>
   </div>
--- VueProject/filter_mixins/src/main.js
+++ VueProject/filter_mixins/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.filter('to-lowercase')
+Vue.filter('to-lowercase', function (value) {
+  return value.toLowerCase()
+})
 
 new Vue({
   el: '#app',
