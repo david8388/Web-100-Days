c91d20272e2cbc78dc858d4f6c62183854d4b2a1
David <hsienwu830808@gmail.com>
2019-11-30 10:28:15 +0800

    An alternative to filters: computed props
    Signed-off-by: David <hsienwu830808@gmail.com>

--- VueProject/filter_mixins/src/App.vue
+++ VueProject/filter_mixins/src/App.vue
@@ -4,6 +4,16 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
         <p>{{ text | toUpperCase | to-lowercase}}</p>
+        <hr>
+        <input v-model="filterText">
+        <ul>
+          <li
+            v-for="fruit in filteredFruits"
+            :key="fruit"
+          >
+            {{ fruit }}
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -13,12 +23,21 @@
 export default {
   data () {
     return {
-      text: 'Hello World'
+      text: 'Hello World',
+      fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+      filterText: ''
     }
   },
   filters: {
     toUpperCase (value) {
       return value.toUpperCase()
+    }
+  },
+  computed: {
+    filteredFruits () {
+      return this.fruits.filter((fruit) => {
+        return fruit.match(this.filterText)
+      })
     }
   }
 }
