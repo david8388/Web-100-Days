4243cb122a784039731d3f27960e132375aec53b
David <hsienwu830808@gmail.com>
2019-12-03 06:51:15 +0800

    Creating a global mixin
    Signed-off-by: David <hsienwu830808@gmail.com>

--- VueProject/filter_mixins/src/main.js
+++ VueProject/filter_mixins/src/main.js
@@ -5,6 +5,13 @@ Vue.filter('to-lowercase', function (value) {
   return value.toLowerCase()
 })
 
+Vue.mixin({
+  created () {
+    // Will output 3 times. main.js render Vue instance, App.vue, List.vue
+    console.log('Global mixin - Created Hook')
+  }
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
