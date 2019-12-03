2ca2d020be436d57ec033dce477f3e3d66441887
David <hsienwu830808@gmail.com>
2019-12-04 06:52:57 +0800

    Mixins and scope
    Signed-off-by: David <hsienwu830808@gmail.com>

--- VueProject/filter_mixins/src/App.vue
+++ VueProject/filter_mixins/src/App.vue
@@ -5,6 +5,7 @@
         <h1>Filters & Mixins</h1>
         <p>{{ text | toUpperCase | to-lowercase}}</p>
         <hr>
+        <button @click="fruits.push('Berries')">Add New Item</button>
         <input v-model="filterText">
         <ul>
           <li
