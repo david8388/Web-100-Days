dd07ffbba087ed27ac1179aee19dc7d9bf29788c
David <hsienwu830808@gmail.com>
2019-12-05 23:14:56 +0800

    Finished exercise_11

--- VueProject/exercise_11/src/App.vue
+++ VueProject/exercise_11/src/App.vue
@@ -1,60 +1,51 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters & Mixins</h1>
+        <!-- Exercise 1) -->
+        <!-- Build a local Filter which reverses the Text it is applied on -->
+        <p> {{ text | reverseText }} </p>
+        <!-- Exercise 2 -->
+        <!-- Build a global Filter which counts the length of a word and it appends it -->
+        <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
+        <p> {{ text | text-append-length }} </p>
+        <!-- Exercise 3 -->
+        <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
+        <p>{{ reverseTextByComputed }}</p>
+        <p>{{ textAppendLengthByComputed }}</p>
+        <!-- Exercise 4 -->
+        <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { textMixins } from './mixins.js'
+
 export default {
-  name: 'app',
+  mixins: [textMixins],
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      text: 'Hello World'
     }
+  },
+  filters: {
+    reverseText (text) {
+      return text.split('').reverse().join('')
+    }
+  },
+  computed: {
+    reverseTextByComputed () {
+      return this.text.split('').reverse().join('')
+    },
+    /*textAppendLengthByComputed () {
+      return this.text + this.text.length
+    }*/
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
--- VueProject/exercise_11/src/main.js
+++ VueProject/exercise_11/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.filter('text-append-length', function (text) {
+  return `${text} (${text.length})`
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
