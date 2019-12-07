984543b6a3fcbb068d065cef12b41d4f60831233
David <hsienwu830808@gmail.com>
2019-12-07 11:00:19 +0800

    Navigating with router links
    Signed-off-by: David <hsienwu830808@gmail.com>

--- VueProject/routing/src/App.vue
+++ VueProject/routing/src/App.vue
@@ -2,7 +2,8 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Routing</h1>
+        <h1>Routing</h1>  
+        <app-header></app-header>
         <hr>
         <router-view></router-view>
       </div>
@@ -11,7 +12,12 @@
 </template>
 
 <script>
+import Header from './components/Header';
+
 export default {
+  components: {
+    appHeader: Header
+  }
 }
 </script>
 
