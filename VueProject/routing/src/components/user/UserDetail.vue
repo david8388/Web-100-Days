02e98b820788c2f45ca464ed24918a3f6fcbde1c
David <hsienwu830808@gmail.com>
2019-12-08 10:29:51 +0800

    Making router links more dynamic
    Signed-off-by: David <hsienwu830808@gmail.com>

--- VueProject/routing/src/components/user/UserDetail.vue
+++ VueProject/routing/src/components/user/UserDetail.vue
@@ -2,5 +2,10 @@
   <div>
     <h3>Some User Details</h3>
     <p>User loaded has ID: {{ $route.params.id }}</p>
+    <router-link
+      tag="button"
+      :to="'/user/' + $route.params.id + '/edit'"
+      class="btn btn-primary"
+    >Edit Link</router-link>
   </div>
 </template>
