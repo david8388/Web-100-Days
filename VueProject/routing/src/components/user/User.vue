f8e71038d07e9d1c44411f65bbc591ecff6273aa
David <hsienwu830808@gmail.com>
2019-12-08 09:14:16 +0800

    Navigating from code
    Signed-off-by: David <hsienwu830808@gmail.com>

--- VueProject/routing/src/components/user/User.vue
+++ VueProject/routing/src/components/user/User.vue
@@ -1,4 +1,20 @@
 <template>
+  <div>
     <h1>The User Page</h1>
-
+    <hr>
+    <button
+      @click="navigateToHome"
+      class="btn btn-primary"
+    >Go to Home</button>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    navigateToHome () {
+      this.$router.push('/')
+    }
+  }
+}
+</script>
