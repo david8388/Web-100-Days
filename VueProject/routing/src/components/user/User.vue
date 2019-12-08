50254f2da3bced1b10a62b37717e11655f7a69f0
David <hsienwu830808@gmail.com>
2019-12-08 09:58:42 +0800

    Setting up child routes
    Signed-off-by: David <hsienwu830808@gmail.com>

--- VueProject/routing/src/components/user/User.vue
+++ VueProject/routing/src/components/user/User.vue
@@ -2,21 +2,16 @@
   <div>
     <h1>The User Page</h1>
     <hr>
-    <p>Loaded ID: {{ id }}</p>
     <button
       @click="navigateToHome"
       class="btn btn-primary"
     >Go to Home</button>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      id: this.$route.params.id
-    }
-  },
   methods: {
     navigateToHome () {
       this.$router.push('/')
--- VueProject/routing/src/routes.js
+++ VueProject/routing/src/routes.js
@@ -1,7 +1,16 @@
 import User from './components/user/User.vue'
+import UserStart from './components/user/UserStart.vue'
+import UserEdit from './components/user/UserEdit.vue'
+import UserDetail from './components/user/UserDetail.vue'
 import Home from './components/Home.vue'
 
 export const routes = [
   { path: '', component: Home },
-  { path: '/user/:id', component: User },
+  {
+    path: '/user', component: User, children: [
+      { path: '', component: UserStart },
+      { path: ':id', component: UserDetail },
+      { path: ':id/edit', component: UserEdit }
+    ]
+  },
 ]
