7e981e372c12a7a58b42f6a01375250b679ff9fe
David <hsienwu830808@gmail.com>
2019-12-11 07:00:33 +0800

    Passing the hash fragment

--- VueProject/routing/src/components/user/UserDetail.vue
+++ VueProject/routing/src/components/user/UserDetail.vue
@@ -4,8 +4,18 @@
     <p>User loaded has ID: {{ $route.params.id }}</p>
     <router-link
       tag="button"
-      :to="{ name: 'userEdit', params: { id: $route.params.id}, query: {locale: 'en', q: 100}}"
+      :to="link"
       class="btn btn-primary"
     >Edit Link</router-link>
   </div>
-</template>s
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      link: { name: 'userEdit', params: { id: this.$route.params.id }, query: { locale: 'en', q: 100 }, hash: '#data' }
+    }
+  }
+}
+</script>
