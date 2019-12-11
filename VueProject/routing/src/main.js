d93af8ef9fa83ac35ad1faba8407e103613e2065
David <hsienwu830808@gmail.com>
2019-12-12 07:15:07 +0800

    Controlling the scroll behavior

--- VueProject/routing/src/main.js
+++ VueProject/routing/src/main.js
@@ -7,7 +7,16 @@ Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes,
-  mode: 'history'
+  mode: 'history',
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    if (to.hash) {
+      return { selector: to.hash }
+    }
+    return { x: 0, y: 0 }
+  }
 })
 new Vue({
   el: '#app',
