af62b1ba323144b00e0a943973c726511d46983e
David <hsienwu830808@gmail.com>
2019-12-13 06:51:23 +0800

    Add button in edit page
    Signed-off-by: David <hsienwu830808@gmail.com>

--- VueProject/routing/src/components/user/UserEdit.vue
+++ VueProject/routing/src/components/user/UserEdit.vue
@@ -3,6 +3,8 @@
     <h3>Edit the User</h3>
     <p>Locale: {{ $route.query.locale }}</p>
     <p>Analytics: {{ $route.query.q }}</p>
+    <hr>
+    <button class="btn btn-primary">Confirm</button>
     <div style="height:700px"></div>
     <p id="data">Some extra data</p>
   </div>
