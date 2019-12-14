3df4e632044eaa135d59f6ab7d41541d3b5a1387
David <hsienwu830808@gmail.com>
2019-12-14 09:21:12 +0800

    Loading Routes lazily

--- VueProject/routing/src/routes.js
+++ VueProject/routing/src/routes.js
@@ -1,10 +1,40 @@
-import User from './components/user/User.vue'
-import UserStart from './components/user/UserStart.vue'
-import UserEdit from './components/user/UserEdit.vue'
-import UserDetail from './components/user/UserDetail.vue'
+// import User from './components/user/User.vue'
+// import UserStart from './components/user/UserStart.vue'
+// import UserEdit from './components/user/UserEdit.vue'
+// import UserDetail from './components/user/UserDetail.vue'
 import Home from './components/Home.vue'
 import Header from './components/Header.vue'
 
+// Udemy
+const User = resolve => {
+  require.ensure(['./components/user/User.vue'], () => {
+    resolve(require('./components/user/User.vue'))
+  }, 'user')
+}
+
+const UserStart = resolve => {
+  require.ensure(['./components/user/UserStart.vue'], () => {
+    resolve(require('./components/user/UserStart.vue'))
+  }, 'user')
+}
+
+const UserEdit = resolve => {
+  require.ensure(['./components/user/UserEdit.vue'], () => {
+    resolve(require('./components/user/UserEdit.vue'))
+  }, 'user')
+}
+
+const UserDetail = resolve => {
+  require.ensure(['./components/user/UserDetail.vue'], () => {
+    resolve(require('./components/user/UserDetail.vue'))
+  }, 'user')
+}
+// LogRocket https://blog.logrocket.com/lazy-loading-in-vue-js/
+/*
+function lazyLoad (view) {
+  return () => import(`@/components/user/${view}.vue`)
+}
+*/
 
 export const routes = [
   {
