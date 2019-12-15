59226b035f749cadc0ffd2185caf3928f91fbd60
David <hsienwu830808@gmail.com>
2019-12-15 09:30:19 +0800

    Mapping getters to props

--- VueProject/vuex/src/components/AnotherResult.vue
+++ VueProject/vuex/src/components/AnotherResult.vue
@@ -1,13 +1,19 @@
 <template>
-  <p>Counter is: {{ counter }}</p>
+  <div>
+    <p>Counter is: {{ doubleCounter }}</p>
+    <p>Number of Clicks: {{ stringCounter }} </p>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   computed: {
-    counter () {
-      return this.$store.getters.doubleCounter
-    }
+    ...mapGetters([
+      'doubleCounter',
+      'stringCounter'
+    ])
   }
 }
 </script>
