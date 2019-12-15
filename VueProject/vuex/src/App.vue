989e827c3d4f85c72d050b03419de88d34a860be
David <hsienwu830808@gmail.com>
2019-12-15 08:56:07 +0800

    Centralized state alone

--- VueProject/vuex/src/App.vue
+++ VueProject/vuex/src/App.vue
@@ -4,6 +4,7 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Vuex</h1>
         <app-result></app-result>
+        <app-another-result></app-another-result>
         <hr>
         <app-counter></app-counter>
       </div>
@@ -12,13 +13,15 @@
 </template>
 
 <script>
-import Counter from './components/Counter.vue';
-import Result from './components/Result.vue';
+import Counter from './components/Counter.vue'
+import Result from './components/Result.vue'
+import AnotherResult from './components/AnotherResult'
 
 export default {
   components: {
     appCounter: Counter,
     appResult: Result,
+    appAnotherResult: AnotherResult
   }
 }
 </script>
