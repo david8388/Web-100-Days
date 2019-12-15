7673450e974fb34141df68924b8eacd3d2943392
David <hsienwu830808@gmail.com>
2019-12-15 10:25:37 +0800

    Using mutations

--- VueProject/vuex/src/store/store.js
+++ VueProject/vuex/src/store/store.js
@@ -14,5 +14,13 @@ export const store = new Vuex.Store({
     stringCounter: state => {
       return state.counter + ' Clicks'
     }
+  },
+  mutations: {
+    increment: state => {
+      state.counter++
+    },
+    decrement: state => {
+      state.counter--
+    }
   }
 })
