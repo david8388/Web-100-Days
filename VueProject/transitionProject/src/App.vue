d504947221d9d15b5bd88f4f76f727385f990f9b
David <hsienwu830808@gmail.com>
2019-12-18 21:43:21 +0800

    Creating a slide transition

--- VueProject/transitionProject/src/App.vue
+++ VueProject/transitionProject/src/App.vue
@@ -16,6 +16,12 @@
             v-if="show"
           >This is some info.</div>
         </transition>
+        <transition name="slide">
+          <div
+            class="alert alert-info"
+            v-if="show"
+          >This is some info.</div>
+        </transition>
       </div>
     </div>
   </div>
@@ -44,5 +50,34 @@ export default {
 .fade-leave-active {
   transition: opacity 1s;
   opacity: 0;
+}
+.slide-enter {
+  /* transform: translateY(20px); */
+}
+.slide-enter-active {
+  animation: slide-in 1s ease-out forwards;
+}
+.slide-leave {
+}
+.slide-enter-active {
+  animation: slide-out 1s ease-out forwards;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(20px);
+  }
 }
 </style>
