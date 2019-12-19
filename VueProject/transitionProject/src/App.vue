0ee19201541c0c95410c5c682475f287cd7085e2
David <hsienwu830808@gmail.com>
2019-12-20 06:43:01 +0800

    Transitioning between multi elements

--- VueProject/transitionProject/src/App.vue
+++ VueProject/transitionProject/src/App.vue
@@ -53,6 +53,21 @@
             v-if="show"
           >This is some info.</div>
         </transition>
+        <transition
+          :name="alertAnimation"
+          mode="out-in"
+        >
+          <div
+            class="alert alert-info"
+            v-if="show"
+            key="info"
+          >This is some info.</div>
+          <div
+            class="alert alert-warning"
+            v-else
+            key="warning"
+          >This is some warning.</div>
+        </transition>
       </div>
     </div>
   </div>
