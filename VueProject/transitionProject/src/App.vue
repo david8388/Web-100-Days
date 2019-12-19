03cd826d08d28b0d12ef70614eb4e8a832901724
David <hsienwu830808@gmail.com>
2019-12-19 21:00:26 +0800

    Using diff css class names

--- VueProject/transitionProject/src/App.vue
+++ VueProject/transitionProject/src/App.vue
@@ -35,6 +35,16 @@
             v-if="show"
           >This is some info.</div>
         </transition>
+        <transition
+          appear
+          enter-active-class="animated bounce"
+          leave-active-class="animated shake"
+        >
+          <div
+            class="alert alert-info"
+            v-if="show"
+          >This is some info.</div>
+        </transition>
       </div>
     </div>
   </div>
