f1d3d70d497b7e0c8bc1899f69aaa039c0a16921
David <hsienwu830808@gmail.com>
2019-12-29 15:26:20 +0800

    Creating an animation in javascript

--- VueProject/transitionProject/src/App.vue
+++ VueProject/transitionProject/src/App.vue
@@ -86,7 +86,7 @@
           :css="false"
         >
           <div
-            style="width:100px;height:100px; background-color:lightgreen"
+            style="width:300px;height:100px; background-color:lightgreen"
             v-if="load"
           ></div>
         </transition>
@@ -101,15 +101,27 @@ export default {
     return {
       show: true,
       load: false,
-      alertAnimation: 'fade'
+      alertAnimation: 'fade',
+      elementWidth: 100
     }
   },
   methods: {
     beforeEnter(el) {
       console.log('beforeEnter')
+      this.elementWidth = 100
+      el.style.width = this.elementWidth + 'px'
     },
     enter(el, done) {
       console.log('enter')
+      let round = 1
+      const interval = setInterval(() => {
+        el.style.width = (this.elementWidth + round * 10) + 'px'
+        round++
+        if (round > 20) {
+          clearInterval(interval)
+          done()
+        }
+      }, 20);
       done()
     },
     afterEnter(el) {
@@ -120,10 +132,20 @@ export default {
     },
     beforeLeave(el) {
       console.log('beforeLeave')
+      this.elementWidth = 300
+      el.style.width = this.elementWidth + 'px'
     },
     leave(el, done) {
       console.log('leave')
-      done(el)
+      let round = 1
+      const interval = setInterval(() => {
+        el.style.width = (this.elementWidth - round * 10) + 'px'
+        round++
+        if (round > 20) {
+          clearInterval(interval)
+          done()
+        }
+      }, 20);
     },
     afterLeave(el) {
       console.log('afterLeave')
