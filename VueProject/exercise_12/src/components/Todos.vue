34fc658b6fa6feca14050e2fd5d2f143065ab86d
David <hsienwu830808@gmail.com>
2020-07-29 14:08:37 +0800

    feat: add todos component

---
+++ VueProject/exercise_12/src/components/Todos.vue
@@ -0,0 +1,25 @@
+<template>
+  <ol>
+    <li
+      v-for="(todo, index) in todos"
+      v-bind:key="index"
+    >
+      {{ todo.text }}
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  data() {
+    return {
+      todos: [
+        {  text: 'Learn Javascript' },
+        {  text: 'Learn Vue' },
+        {  text: 'Build something awesome' }
+      ]
+    }
+  }
+}
+</script>
