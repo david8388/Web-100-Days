77ce496d778017d1e4e571be0cc5e7142dad55df
David <hsienwu830808@gmail.com>
2020-08-31 15:01:22 +0800

    feat: add observable.js

---
+++ RxJS/observable.js
@@ -0,0 +1,45 @@
+class Producer {
+    constructor() {
+        this.listeners = []
+    }
+
+    addListener(listener) {
+        if (typeof listener === 'function' ) {
+            this.listeners.push(listener)
+        } else {
+            throw new Error('listener must be function')
+        }
+    }
+
+    removeListener(listener) {
+        const result = this.listeners.filter(x => x !== listener)
+        this.listeners = result
+        return result
+    } 
+
+    notify(message) {
+        this.listeners.forEach(listener => {
+            listener(message)
+        })
+    }
+
+}
+
+const producer = new Producer()
+
+const listener1 = (message) => {
+    console.log(`Hi, I'm Listener1, Message is :${message}`);
+}
+
+const listener2 = (message) => {
+    console.log(`Hi, I'm Listener2, Message is :${message}`);
+}
+
+producer.addListener(listener1)
+producer.addListener(listener2)
+
+producer.notify('This is a test message')
+
+// The following code will cause an error
+// const listener3 = 10
+// producer.addListener(listener3) 
